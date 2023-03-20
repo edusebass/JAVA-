19b37d6331db1b3207f6c2344a10146e08cc2e07
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-20 13:41:46 -0500

    Update 2. Variables.java

--- 2. Variables.java
+++ 2. Variables.java
@@ -1,4 +1,4 @@
-//                      VARIABLES     
+//                      TYPE VARIABLES     
 // Every variable has a type, which defines the type of the value it holds.
 //A variable can hold a text value, a number, a decimal, etc.
 
@@ -20,3 +20,21 @@ class Demo {
 
 //observation: The variable can change if you asign multiples lines each variable. 
 //In this case name = "James" but before name = "David", in the console will show David.
+
+//                           MORE TYPE VARIABLES
+/*
+int number = 23;
+----------------------------
+double height = 223.34;
+float weight = 223.435f; // you need to use f after the value
+                         double vs float
+ As a general rule: use float instead of double when memory usage is critical. 
+If you need more precise computations, for example, when dealing with currency, use double.
+----------------------------
+char letter = 'b'; //The char type is used to hold a single character. Always with simple quotes.
+----------------------------
+boolean isOpen = false; // Another important type is boolean.
+It can hold only the values true or false.
+This is handy when we work with states or conditions.
+----------
+*/
