cf9277a93c28cd2ac7357585214d1f8268b56b8d
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-20 11:23:21 -0500

    Create 1. Basic structure.java

---
+++ 1. Basic structure.java
@@ -0,0 +1,12 @@
+//BASIC STRUCTURE JAVA
+
+class Main { //Java always with "class", you can call to class as you want.
+  public static void main(String[] args) { //Java works with function main
+    //"out" shows a text in the console
+    System.out.println("Hello world! esta es una funcion de Java");
+  }
+}
+
+//Always each code line finished with semicolon (;)
+//Always each class and function will have brackets
+//Before you will learn more about of class and funcionts
