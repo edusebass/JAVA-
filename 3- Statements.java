72d5e17530ef0b414ca6d6ff0ebf23c1faa5210d
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-21 16:07:10 -0500

    Create 3- Statements.java

---
+++ 3- Statements.java
@@ -0,0 +1,11 @@
+public class HideAndSeek {
+  public static void main(String[] args) {
+    //println() will print in other line. 
+    System.out.println("Let's play hide and seek.");
+    //print() will print in the same line. 
+    System.out.print("Three...");
+    System.out.print("Two...");
+    System.out.println("One...");
+    System.out.println("Ready or not, here I come!");
+  }
+}
