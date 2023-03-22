54a73a4f6a78f8c1a672ca92076867e97e841c50
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-22 17:26:48 -0500

    Create 4- First program in JAVA.java

---
+++ 4- First program in JAVA.java
@@ -0,0 +1,9 @@
+//It is a first and basic program in Java
+//It has a public class an public static void main and a print in the console.
+
+public class Review {
+  public static void main(String[] args){
+    //The main method executes the tasks of the class
+    System.out.println("My first Java program from scratch!");
+  }
+}
