38b4ee6e449a50090bf15474295d94170ae01e90
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-04-03 14:47:35 -0500

    Create 6- Class.java

---
+++ 6- Class.java
@@ -0,0 +1,13 @@
+public class Car {
+// scope of Car class starts after curly brace
+ 
+  public static void main(String[] args) {
+    // scope of main() starts after curly brace
+ 
+    // program tasks
+ 
+  }
+  // scope of main() ends after curly brace
+ 
+}
+// scope of Car class ends after curly brace
