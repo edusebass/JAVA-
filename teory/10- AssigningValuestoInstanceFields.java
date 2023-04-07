689a69bd6bac22676bba25efece7fd47c503f15d
edusebass <eduardoalmachi123@hotmail.com>
2023-04-06 20:40:42 -0500

    update

--- teory/10- AssigningValuestoInstanceFields.java
+++ teory/10- AssigningValuestoInstanceFields.java
@@ -8,6 +8,7 @@ public class Store {
   }
   
   // main method
+
   public static void main(String[] args) {
     Store lemonadeStand = new Store("lemonade");
     System.out.println(lemonadeStand.productType);
