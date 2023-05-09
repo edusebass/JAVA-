da25ea599f7499c5032ca7ff06d1210b51320eaf
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-05-09 09:19:13 -0500

    Create 1.1 first programa.java

---
+++ teory/1.1 first programa.java
@@ -0,0 +1,30 @@
+import java.util.Scanner; // program uses class Scanner
+public class Addition {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1; // first number to add
+        int number2; // second number to add
+        int sum; // sum of number1 and number2Scanner input = new Scanner(System.in);
+
+        int res;
+        int multi;
+        int divi;
+
+        System.out.println("Enter first integer: ");
+        number1 = input.nextInt(); // read first number from user
+
+        System.out.print("Enter second integer: "); // prompt
+        number2 = input.nextInt(); // read second number from user
+
+        sum = number1 + number2; // add numbers, then store total in sum
+        res = number1 - number2;
+        multi = number1 * number2;
+        divi = number1 / number2;
+
+        System.out.printf("La suma es: %d%n", sum); // display sum
+        System.out.printf("La resta es: %d%n", res);
+        System.out.printf("La multiplicación es: %d%n", multi);
+        System.out.printf("La division es: %d%n", divi);
+
+    }
+}
